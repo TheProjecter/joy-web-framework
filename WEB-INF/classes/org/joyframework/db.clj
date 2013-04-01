cc104b19b7d7be85d815e8101afa6ccd17e1c606
ypy6448 <ypy6448@gmail.com>
2013-04-01 21:27:23 +0000

    init upload.

---
+++ WEB-INF/classes/org/joyframework/db.clj
@@ -0,0 +1,38 @@
+; Copyright (c) Pengyu Yang. All rights reserved
+
+(ns org.joyframework.db
+  (:require [clojure.string :as str])
+  (:import com.jolbox.bonecp.BoneCPDataSource))
+
+
+(defmacro defds [name spec]
+  "Defines a var after the given name pointing to a datasource
+   required by the spec."
+  `(def ~name (ds ~spec)))
+
+
+(defn ds [spec]
+  "Returns a datasource by the spec. The argument is a map, 
+   which may contain following keys:
+   :driver      --> the JDBC driver class
+   :subprotocol
+   :subname
+   :user        --> the name of user accessing the database, sa by default
+   :password    --> the password of the user, empty by default
+   :jndi        --> the JNDI pointing to a defined by the container,
+                    it has priority over other options. "   
+  (let [{:keys [driver subprotocol subname user password jndi]
+         :or {user "sa" password ""}} spec]
+    (if jndi
+      (do (println "----> jndi: " jndi))
+      (do
+        (println "----> Create a BoneCP datasource.")
+        (Class/forName driver)
+        (doto (BoneCPDataSource.)
+          (.setJdbcUrl
+           (str "jdbc:" subprotocol ":" subname))
+          (.setUsername user)
+          (.setPassword password))
+        ))
+    )
+  )
